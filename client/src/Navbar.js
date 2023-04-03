25c157323b6bd836b4ffeb14fae2a36e21c7041e
Procrastinatorinator <onehundredpercentnotcertain@gmail.com>
2023-04-03 14:33:57 -0500

    update navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+
 
 export default function Navbar() {
+
+  const [showlist, setToggle] = useState(false)
   return (
     <nav className=' bg-HotelWater p-5  shadow md:flex md:items-center md:justify-between text-white'>
-        <div>
+        {/* HOTEL LOGO/NAME */}
+        <div className='flex'>
             <span className=' text-4xl font-[Poppins] cursor-pointer'>
                 ~HotelName~
             </span>
+            <div className='ml-5 text-lg'>
+                <button onClick={() => setToggle(!showlist)} className=' bg-white text-cyan-900 p-2 duration-500 rounded font-[Poppins] inline-flex items-center hover:bg-slate-200'>
+                    Button Here
+                </button>
+                {showlist && (
+                    <div className='mt-1 shadow-lg shadow-HotelGrey bg-white rounded absolute' id='Drowdown'>
+                        <a className='block text-black p-1 hover:bg-HotelGrey cursor-pointer'>Deals</a>
+                        <a className='block text-black p-1 hover:bg-HotelGrey cursor-pointer'>Car Rentals</a>
+                        <a className='block text-black p-1 hover:bg-HotelGrey cursor-pointer'>Groups & Meetings</a>
+                        <a className='block text-black p-1 hover:bg-HotelGrey cursor-pointer'>Gift Cards</a>
+                    </div>
+                )}
+            </div>
         </div>
+
+        {/* DROP DOWN BUTTON */}
+        
+        
+        {/* SIDE LINKS */}
         <ul className='md:flex md:items-center hidden'>
             <li className=' mx-4'>
-                <a href={''}>Login</a>
+                <a href={'/login'}>Login</a>
             </li>
             <li className=' mx-4'>
                 <a href={''}>Support</a>
@@ -18,10 +41,11 @@ export default function Navbar() {
             <li className=' mx-4'>
                 <a href={''}>Trips</a>
             </li>
-            <button className=' bg-white text-cyan-900 p-2 duration-500 rounded font-[Poppins] hover:bg-slate-200 mx-2'>
-                Button Here
-            </button>
         </ul>
+        <button className=' bg-white text-cyan-900 p-2 duration-500 rounded font-[Poppins] hover:bg-slate-200 md:hidden'>
+            Button Here
+        </button>
+        
     </nav>
   )
 }
